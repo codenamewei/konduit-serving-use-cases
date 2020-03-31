0c9f50fc08d52de0b2956a9978331b4b96675410
codenamewei <codenamewei@gmail.com>
2020-03-31 22:53:13 +0800

    Get 3-2-DL4J-LSTM-PythonClient working

--- 3-2-DL4J-LSTM-PythonClient/client.py
+++ 3-2-DL4J-LSTM-PythonClient/client.py
@@ -6,45 +6,15 @@ from konduit.client import Client
 from konduit.load import client_from_file
 import time
 
-current_milli_time = lambda: int(round(time.time() * 1000))
-
-port = 65322
-
 client = client_from_file("config.yaml")
 
-print('Load file for class index <> class label')
-label_path = "labelclass.pickle"
-labelhandler = open(label_path, 'rb')
-labelhandler = pickle.load(labelhandler)
+root_path = "C:\\Users\\chiaw\\Documents\\data\\konduit-serving-use-cases\\3-2-DL4J-LSTM-PythonClient\\"
+featureFile = root_path + "alt-atheism-feature.npy"
+featureMaskFile = root_path + "alt-atheism-featureMask.npy"
 
-root_path = "D:\\Users\\chiawei\\Documents\\data\\newsgroup_data\\20news-bydate\\"
-f = open (root_path + "20news-bydate-test\\alt.atheism\\53280", "r")
+feature = np.load(featureFile, encoding='bytes', allow_pickle=True)
+featureMask = np.load(featureMaskFile, encoding='bytes', allow_pickle=True)
 
-contents = f.read()
-
-input_param = {"String": contents}
-
-total_count = 5
-iteration = range(total_count)
-
-before_milli_time = current_milli_time()
-
-prediction = client.predict({client.input_names[0]: input_param})
+prediction = client.predict({client.input_names[0]: feature})
 
 print(prediction)
-
-output = np.array(prediction[client.output_names[0]]["ndArray"]["data"])
-
-index = int(np.argmax(output, 0))
-
-print("Class: {}".format(labelhandler[index]))
-print("Probabilities: {}\n".format(np.max(output[index])))
-
-after_milli_time = current_milli_time()
-
-total_time = after_milli_time - before_milli_time
-milli_average = total_time / float(total_count)
-
-print("Total time taken: {} ms".format(total_time))
-print("Average time per request: {0:.2f} ms/request".format(milli_average))
-print("Average time per request: {0:.2f} s/request".format(milli_average * 0.001))
